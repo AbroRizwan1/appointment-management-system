e002156566e154e5a408b9032b66e1987e932178
AbroRizwan1 <rizwanabro14@example.com>
2026-02-24 15:21:03 +0500

    Updated login page routing and fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
 
 const repoName = "/appointment-management-system"; // GitHub repo name
 
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter basename={repoName}>
+    <BrowserRouter basename={repoName}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
